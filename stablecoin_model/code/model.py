eb06d94a5a41edad59875dca28df15a8ef115505
sumin-xna <169062717+sumin-xna@users.noreply.github.com>
2026-04-08 17:38:38 -0400

    Delete stablecoin_model directory

--- stablecoin_model/code/model.py
+++
@@ -1,9 +0,0 @@
-# Core hedge-run model structure
-
-import numpy as np
-
-def hedge_demand(theta, cost, chi_h, eta_h):
-    return max(0.0, chi_h * theta - eta_h * cost)
-
-def run_demand(pi, omega_s, gamma_r, psi_info, pi0, tau):
-    return gamma_r * (1 + psi_info * omega_s) * (1 / (1 + np.exp(-(pi - pi0)/tau)))
